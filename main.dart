d3e50fac46abbc907bab91faebf48d8d2408f990
rkafeli <140311727+rkafeli@users.noreply.github.com>
2023-07-23 16:46:46 +0100

    Add files via upload
    ok

--- main.dart
+++ main.dart
@@ -1,125 +1,103 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
+class PlantIdentification extends StatefulWidget {
+  @override
+  _PlantIdentificationState createState() => _PlantIdentificationState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _PlantIdentificationState extends State<PlantIdentification> {
+  File? _image;
+  String? _result;
+  Interpreter? _interpreter;
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  void initState() {
+    super.initState();
+    loadModel();
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Future<void> loadModel() async {
+    try {
+      var appDir = await getApplicationDocumentsDirectory();
+      var modelPath = join(appDir.path, 'model.tflite');
+      _interpreter = await Interpreter.fromAsset(modelPath);
+    } catch (e) {
+      print('Error loading model: $e');
+    }
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  Future<void> _predictImage() async {
+    if (_image == null || _interpreter == null) return;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    var imageBytes = await _image!.readAsBytes();
+    var input = imageBytes.buffer.asUint8List();
 
-  final String title;
+    var output = List.filled(_interpreter!.getOutputTensor(0).shape.reduce((a, b) => a * b), 0).buffer.asUint8List();
+    _interpreter!.run(input, output);
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+    var labels = await File('assets/labels.txt').readAsLines();
+    var maxIndex = output.indexOf(output.reduce((curr, next) => curr > next ? curr : next));
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++ = _counter + 2;
+      _result = labels[maxIndex];
     });
   }
 
+  Future<void> _getImage() async {
+    final picker = ImagePicker();
+    var pickedImage = await picker.pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      setState(() {
+        _image = File(pickedImage.path);
+        _result = null;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Plant Identification'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            _image == null
+                ? Text('Select an image')
+                : Image.file(
+                    _image!,
+                    height: 200,
+                  ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _getImage,
+              child: Text('Select Image'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            SizedBox(height: 20),
+            _result == null
+                ? Container()
+                : Text(
+                    'Prediction: $_result',
+                    style: TextStyle(fontSize: 20),
+                  ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _predictImage,
+        child: Icon(Icons.check),
+      ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: PlantIdentification(),
+  ));
 }
